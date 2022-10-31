898f7afba177b57381c6f85991bbd91758cccb72
zhuangxiaowei <zhuangxiaowei@bluemoon.com.cn>
2022-10-31 13:41:39 +0800

    feat：select slots

--- src/components/SearchFilter/components/Select.vue
+++ src/components/SearchFilter/components/Select.vue
@@ -1,3 +1,25 @@
 <template>
-  <a-select></a-select>
+  <div class="content">
+    <a-select style="width: 100%">
+      <!-- antd插槽 -->
+      <template v-for="slot in antdSlots" #[slot]>
+        <slot :name="slot"></slot>
+      </template>
+    </a-select>
+
+    <!-- 自定义插槽 -->
+    <div v-if="customSlots.includes('tips')">
+      <slot name="tips"></slot>
+    </div>
+
+    <!-- other custom slots...... -->
+  </div>
 </template>
+
+<script setup>
+import { useSlots } from "vue";
+import { getSlots } from "./hooks";
+
+const $slots = useSlots();
+const { antdSlots, customSlots } = getSlots($slots, "input");
+</script>
