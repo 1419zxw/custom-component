6a6d6b6b15b3e1c20c499f9df4722dc8b1f61555
庄晓伟 <zhuangxiaowei@bluemoon.com.cn>
2022-11-05 13:37:06 +0800

    feat：常量

--- src/components/SearchFilter/constant.js
+++ src/components/SearchFilter/constant.js
@@ -1,6 +1,12 @@
-// item默认左外边距
-export const DEFAULT_ITEM_MARGIN_LEFT = '10px'
+// container内边距
+export const CONTAINER_PADDING = "20px";
+// item左外边距
+export const ITEM_MARGIN_LEFT = "10px";
 // label默认宽度
-export const DEFAULT_ITEM_LABEL_WIDTH = '80px'
+export const DEFAULT_ITEM_LABEL_WIDTH = "80px";
 // comp默认宽度
-export const DEFAULT_ITEM_COMP_WIDTH = '200px'
+export const DEFAULT_ITEM_COMP_WIDTH = "200px";
+// 按钮组宽度（有高级查询）
+export const BUTTON_GROUP_WIDTH_1 = "250px";
+// 按钮组宽度（无高级查询）
+export const BUTTON_GROUP_WIDTH_2 = "160px";
