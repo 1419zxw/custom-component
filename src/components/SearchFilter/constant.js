199924dafc344c5368061f2e1310806613d24859
庄晓伟 <zhuangxiaowei@bluemoon.com.cn>
2022-11-05 11:16:43 +0800

    feat：一些样式及常量

---
+++ src/components/SearchFilter/constant.js
@@ -0,0 +1,6 @@
+// item默认左外边距
+export const DEFAULT_ITEM_MARGIN_LEFT = '10px'
+// label默认宽度
+export const DEFAULT_ITEM_LABEL_WIDTH = '80px'
+// comp默认宽度
+export const DEFAULT_ITEM_COMP_WIDTH = '200px'
--- src/components/SearchFilter/index.vue
+++ src/components/SearchFilter/index.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="container">
-    <div class="container_item" v-for="comp in config" :key="comp.prop">
-      <span class="container_item_label">{{ comp.label }}</span>
+    <div v-for="comp in config" :key="comp.prop" :style="{ marginLeft: DEFAULT_ITEM_MARGIN_LEFT }" class="container_item">
+      <span
+        class="container_item_label"
+        :style="{ width: comp.labelWidth || DEFAULT_ITEM_LABEL_WIDTH }"
+        >{{ `${comp.label}：` }}</span
+      >
       <component
         :is="getComponent(comp.type)"
         v-model:value="model[comp.prop]"
         v-bind="comp.attribute"
         v-on="comp.events"
+        :style="{ width: comp.compWidth || DEFAULT_ITEM_COMP_WIDTH }"
         class="container_item_comp"
       >
         <template
@@ -21,8 +26,9 @@
 </template>
 
 <script setup>
-import { defineProps } from "vue";
-import componentMap from "./components/componentMap";
+import { defineProps } from "vue"
+import componentMap from "./components/componentMap"
+import { DEFAULT_ITEM_LABEL_WIDTH, DEFAULT_ITEM_COMP_WIDTH, DEFAULT_ITEM_MARGIN_LEFT } from "./constant"
 
 const props = defineProps({
   model: {
@@ -33,35 +39,33 @@ const props = defineProps({
     type: Array,
     default: () => [],
   },
-});
+})
 
 const getComponent = (type) => {
-  return componentMap[type];
-};
+  return componentMap[type]
+}
 
 const getSlots = (slotsObj) => {
-  if (!slotsObj) return [];
+  if (!slotsObj) return []
 
   return Object.entries(slotsObj).map((item) => {
     return {
       name: item[0],
       value: item[1],
-    };
-  });
-};
+    }
+  })
+}
 </script>
 
 <style lang="less">
 .container {
   display: flex;
+  padding: 20px;
   &_item {
     display: flex;
-    width: 300px;
+    align-items: center;
     &_label {
-      width: 60px;
-    }
-    &_comp {
-      flex: 1;
+      text-align: right;
     }
   }
 }
